c4e3036c166ee7dee3e05191f06680321f1dbaca
CruvixDev <100412344+CruvixDev@users.noreply.github.com>
2025-11-16 15:22:03 +0100

    Give Windows PATH variables to vcpkg

--- cmake/gcc-arm-none-eabi.cmake
+++ cmake/gcc-arm-none-eabi.cmake
@@ -1,31 +1,23 @@
-set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_NAME               Generic)
+set(CMAKE_SYSTEM_PROCESSOR          arm)
 
-# Find arm none eabi toolchain path
-find_program(ARM_GCC arm-none-eabi-gcc)
-message("Found ARM GCC: " ${ARM_GCC})
-find_program(ARM_GXX arm-none-eabi-g++)
-message("Found ARM G++: " ${ARM_GXX})
-find_program(ARM_OBJCOPY arm-none-eabi-objcopy)
-message("Found ARM OBJCOPY: " ${ARM_OBJCOPY})
-find_program(ARM_SIZE arm-none-eabi-size)
-message("Found ARM SIZE: " ${ARM_SIZE})
+# Some default GCC settings
+# arm-none-eabi- must be part of path environment
+set(TOOLCHAIN_PREFIX                arm-none-eabi-)
+set(FLAGS                           "-fdata-sections -ffunction-sections -Wl,--gc-sections")
+set(CPP_FLAGS                       "${FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
 
-# Default GCC settings
-set(FLAGS "-fdata-sections -ffunction-sections -Wl,--gc-sections")
-set(CPP_FLAGS "${FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
+set(CMAKE_C_FLAGS                   ${FLAGS})
+set(CMAKE_CXX_FLAGS                 ${CPP_FLAGS})
 
-set(CMAKE_C_FLAGS ${FLAGS})
-set(CMAKE_CXX_FLAGS ${CPP_FLAGS})
+set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
+set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++)
+set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy)
+set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size)
 
-set(CMAKE_C_COMPILER ${ARM_GCC})
-set(CMAKE_ASM_COMPILER ${ARM_GCC})
-set(CMAKE_CXX_COMPILER ${ARM_GXX})
-set(CMAKE_OBJCOPY ${ARM_OBJCOPY})
-set(CMAKE_SIZE ${ARM_SIZE})
-
-set(CMAKE_EXECUTABLE_SUFFIX_ASM ".elf")
-set(CMAKE_EXECUTABLE_SUFFIX_C ".elf")
-set(CMAKE_EXECUTABLE_SUFFIX_CXX ".elf")
+set(CMAKE_EXECUTABLE_SUFFIX_ASM     ".elf")
+set(CMAKE_EXECUTABLE_SUFFIX_C       ".elf")
+set(CMAKE_EXECUTABLE_SUFFIX_CXX     ".elf")
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
