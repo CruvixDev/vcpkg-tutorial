591877c0cee85031e360ad1e8b0b093fd7781957
Julien Cruvieux <100412344+CruvixDev@users.noreply.github.com>
2025-11-18 15:07:26 +0100

    Replace hardcoded version in profiles by VERSION variable

--- ports/cmsis-device-l5/portfile.cmake
+++ ports/cmsis-device-l5/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO STMicroelectronics/cmsis-device-l5
-    REF v1.0.6
+    REF v${VERSION}
     SHA512 7bb3ec39b6492fab3ebcc4292f38fe1906d90e04d3e6047dcfeb93e111b9bf58ec14cb81072346569f05dd4543c2383aecebc2af75bd9cbf7af2da0876cf655d
     HEAD_REF master
 )
--- ports/stm32l5xx-hal-driver/portfile.cmake
+++ ports/stm32l5xx-hal-driver/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO STMicroelectronics/stm32l5xx-hal-driver
-    REF v1.0.6
+    REF v${VERSION}
     SHA512 bfcde46afde10e4b02f34abcee7355274cd6d599679cb993b24cb3630afe8a2dc00cfe2ed7736a5032e2cbc423cd530c06b8db1de7ba65ba6051c998d04f8bed
     HEAD_REF master
 )
